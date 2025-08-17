9cdf584dba7af901a71c52404ae4d466efdc2ef2
Naveen Prakash.r <lightshinichi2000@gmail.com>
2025-08-17 20:48:58 +0100

    Updating data validation module

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -13,7 +14,13 @@ if __name__== "__main__" :
         ingest= DataIngestion(data_cofig)
         logging.info("Intitate the data ingestion")
         artifact= ingest.initiate_data_ingestion()
-        print(artifact)
+        logging.info("Data Initiation Completed")
+        data_valid_config= DataValidationConfig(tpipe)
+        data_validation = DataValidation(artifact,data_valid_config)
+        logging.info("Initiate data validation")
+        data_valid_artifact= data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_valid_artifact)
 
 
     except Exception as e :
