65eeab7d279da03923aab4bc8e6d57749333cbea
Naveen Prakash.r <lightshinichi2000@gmail.com>
2025-08-18 09:43:49 +0100

    Updating data transformation pipeline

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -21,6 +22,13 @@ if __name__== "__main__" :
         data_valid_artifact= data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(data_valid_artifact)
+        logging.info("Data Transformation Initiated")
+        data_transformation_config = DataTransformationConfig(tpipe)
+        logging.info("Data Transformation Completed")
+        data_transformation = DataTransformation(data_valid_artifact,data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+
 
 
     except Exception as e :
